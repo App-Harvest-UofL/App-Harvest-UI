84804a909f0fc08999c898ac1988ea56bdbaf928
zanes <sorensenzane@gmail.com>
2023-03-01 17:50:54 -0500

    test login

---
+++ src/Login.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch('http://localhost:5164/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.success) {
+        // Show success message to user
+        document.getElementById('message').innerHTML = 'Login successful!';
+      } else {
+        // Show error message to user
+        document.getElementById('message').innerHTML = 'Login failed. Please check your credentials.';
+      }
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors that occurred during the fetch call
+    console.error(error);
+  });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default LoginForm
