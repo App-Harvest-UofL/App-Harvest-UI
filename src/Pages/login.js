6c48d9b437d40561a0e3cad454cd51c0575e7092
Collin Shields <cmshie05@louisville.edu>
2023-04-05 13:38:22 -0400

    update to lgoing page, added icons and buttons

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -83,24 +83,29 @@ const LoginPage = () => {
               <form onSubmit={handleSubmit}>
                 <div className='d-flex justify-content-center'>
                   <input
-                    className='input-style mt-2 mb-2'
+                    className='input-style input-style-email mt-2 mb-2'
                     type='email'
                     placeholder='email'
                     ref={emailRef}
                     onChange={(e) => setEmail(e.target.value)}
                     value={email}
                     required
-                  ></input>
+                  />
                 </div>
                 <div className='d-flex justify-content-center'>
                   <input
-                    className='input-style mt-2 mb-2'
+                    className='input-style input-style-password mt-2 mb-2'
                     type='password'
                     placeholder='password'
                     onChange={(e) => setPassword(e.target.value)}
                     value={password}
                     required
                   ></input>
+                </div>
+                <div className='d-flex w-100 justify-content-center'>
+                  <a classname='' href='/'>
+                    Reset Password
+                  </a>
                 </div>
                 <div className='d-flex justify-content-center'>
                   <button className='login-button-style' type='submit'>
@@ -113,16 +118,12 @@ const LoginPage = () => {
                       />
                     </span>
                   </button>
-                  <button className='login-button-style' type='submit'>
-                    Register
-                    <span className='ms-5'>
-                      <img
-                        className='right-arrow-style'
-                        src='arrow-right-solid.svg'
-                        alt='right arrow shaped image'
-                      />
-                    </span>
-                  </button>
+                </div>
+                <div className='d-flex justify-content-center'>
+                  <p>
+                    Don't have an account?{' '}
+                    <a href='/registerPage'>Register Here</a>
+                  </p>
                 </div>
               </form>
             </div>
