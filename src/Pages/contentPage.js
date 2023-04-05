d25359a7ca824c94a91a62843b69932c39d392e7
zanes <sorensenzane@gmail.com>
2023-04-05 13:48:36 -0400

    added stuff for get file

--- src/Pages/contentPage.js
+++ src/Pages/contentPage.js
@@ -1,13 +1,35 @@
 /** @format */
 
-import React from 'react';
+import React, { useState } from 'react';
+import axios from '../API Pull/axios';
 
 const contentPage = () => {
+  const [fileName, setFileName] = useState('');
+  const getFile = async () => {
+    try {
+      const response = await axios.get("http://testtesttest123.com/?hi", {
+        params: { fileName }
+      })
+    }
+    catch (err)
+    {
+      if (!err?.response) {
+        setError('No server response');
+      } else if (err.response?.statuse === 400) {
+        setError('Invalid email or password.');
+      } else if (err.response?.status === 401) {
+        setError('Unauthorized');
+      } else {
+        setError('Something went wrong. Please try again later.');
+      }      
+    }
+  }
   return (
     <div>
       <h1>Content Page</h1>
     </div>
   );
 };
+
 
 export default contentPage;
