644c50e17a80aac0f8d5707b377251f7292f9e2e
zanes <sorensenzane@gmail.com>
2023-04-06 22:31:42 -0400

    Added basic html for content

--- src/Pages/contentPage.js
+++ src/Pages/contentPage.js
@@ -3,10 +3,35 @@
 import React from 'react';
 
 const contentPage = () => {
+  /* put array of data calls somehow here */ 
   return (
-    <div>
-      <h1>Content Page</h1>
-    </div>
+    <html>
+      <head>
+        <title>Downloadable Content</title>
+      </head>
+      <body>
+        <h1>Downloadable Content</h1>
+        <ul>
+          <li>
+            <h2>PDFs</h2>
+            <ul>
+              <li><a href="path/to/pdf1.pdf" download>PDF 1</a></li>
+              <li><a href="path/to/pdf2.pdf" download>PDF 2</a></li>
+              <li><a href="path/to/pdf3.pdf" download>PDF 3</a></li>
+            </ul>
+          </li>
+          <li>
+            <h2>PowerPoint</h2>
+            <ul>
+              <li><a href="path/to/image1.jpg" download>powerpoint1</a></li>
+              <li><a href="path/to/image2.jpg" download>powerpoint2</a></li>
+              <li><a href="path/to/image3.jpg" download>powerpoint3</a></li>
+            </ul>
+          </li>
+        </ul>
+      </body>
+    </html>
+    
   );
 };
 
