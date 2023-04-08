1abbaae429e94f5a010baa0421fed32fd9b56734
daviswest <90291673+daviswest@users.noreply.github.com>
2023-04-08 13:10:55 -0400

    Something like this for content page
    Needs styling and more

--- src/Pages/contentPage.js
+++ src/Pages/contentPage.js
@@ -1,38 +1,50 @@
 /** @format */
 
-import React from 'react';
+import React, { useState } from "react";
 
-const contentPage = () => {
-  /* put array of data calls somehow here */ 
+const ContentPage = () => {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+
+  const handleFileUpload = (event) => {
+    setSelectedFiles(event.target.files);
+  };
+
+  const handleFileDownload = (url) => {
+    window.open(url);
+  };
+
   return (
-    <html>
-      <head>
-        <title>Downloadable Content</title>
-      </head>
-      <body>
-        <h1>Downloadable Content</h1>
-        <ul>
-          <li>
-            <h2>PDFs</h2>
-            <ul>
-              <li><a href="path/to/pdf1.pdf" download>PDF 1</a></li>
-              <li><a href="path/to/pdf2.pdf" download>PDF 2</a></li>
-              <li><a href="path/to/pdf3.pdf" download>PDF 3</a></li>
-            </ul>
-          </li>
-          <li>
-            <h2>PowerPoint</h2>
-            <ul>
-              <li><a href="path/to/image1.jpg" download>powerpoint1</a></li>
-              <li><a href="path/to/image2.jpg" download>powerpoint2</a></li>
-              <li><a href="path/to/image3.jpg" download>powerpoint3</a></li>
-            </ul>
-          </li>
-        </ul>
-      </body>
-    </html>
-    
+    <div>
+      <h1>File Uploader/Downloader</h1>
+      <input type="file" onChange={handleFileUpload} multiple />
+      <br />
+      <br />
+      <h2>Uploaded Files:</h2>
+      <ul>
+        {selectedFiles.length > 0 &&
+          Array.from(selectedFiles).map((file, index) => (
+            <li key={index}>
+              {file.name} ({file.type}, {file.size} bytes)
+            </li>
+          ))}
+      </ul>
+      <br />
+      <h2>Downloaded Files:</h2>
+      <ul>
+        <li>
+          <a href="https://example.com/file1.pdf" onClick={() => handleFileDownload("https://example.com/file1.pdf")}>
+            file1.pdf
+          </a>
+        </li>
+        <li>
+          <a href="https://example.com/file2.jpg" onClick={() => handleFileDownload("https://example.com/file2.jpg")}>
+            file2.jpg
+          </a>
+        </li>
+        {/* Add more files as needed */}
+      </ul>
+    </div>
   );
 };
 
-export default contentPage;
+export default ContentPage;
